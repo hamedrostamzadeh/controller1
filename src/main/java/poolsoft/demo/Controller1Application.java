7266fa58f3eea6b340b9210567b5d1229018432f
Hamed Rostamzadeh <hamed.rostamzadeh@voxcarrier.com>
2022-02-23 02:21:12 +0330

    c1

--- src/main/java/poolsoft/demo/Controller1Application.java
+++ src/main/java/poolsoft/demo/Controller1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Controller1Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Controller1Application.class, args);
     }
