9da5305a5b55485d07b0f08087fb0487490aa022
Hamed Rostamzadeh <hamed.rostamzadeh@voxcarrier.com>
2022-02-23 02:14:54 +0330

    init controller1

---
+++ src/test/java/poolsoft/demo/Controller1ApplicationTests.java
@@ -0,0 +1,13 @@
+package poolsoft.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Controller1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
