613826858eebf70c547f18c75f1b6d31bd8f1733
Hamed Rostamzadeh <hamed.rostamzadeh@voxcarrier.com>
2022-02-23 03:52:33 +0330

    working fine.

--- src/main/java/poolsoft/demo/Controller1Application.java
+++ src/main/java/poolsoft/demo/Controller1Application.java
@@ -2,12 +2,14 @@ package poolsoft.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import poolsoft.model.M;
 
 @SpringBootApplication
 public class Controller1Application {
 
 
     public static void main(String[] args) {
+        M m = new M();
         SpringApplication.run(Controller1Application.class, args);
     }
 
