a0f20e1903114ba7548ced38a4ac31ba1af9a44e
Hamed Rostamzadeh <hamed.rostamzadeh@voxcarrier.com>
2022-02-23 04:01:56 +0330

    m1

--- src/main/java/poolsoft/demo/Controller1Application.java
+++ src/main/java/poolsoft/demo/Controller1Application.java
@@ -2,14 +2,14 @@ package poolsoft.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import poolsoft.model.M;
+import poolsoft.model.M1;
 
 @SpringBootApplication
 public class Controller1Application {
 
 
     public static void main(String[] args) {
-        M m = new M();
+        M1 m1 = new M1();
         SpringApplication.run(Controller1Application.class, args);
     }
 
